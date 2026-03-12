5c4d34d1814a80a9902332dc1140fd8c45583e6e
Jhony Bosio <2fa.protegido@gmail.com>
2026-03-11 21:49:17 -0300

    chore: professional branding, favicon and stray tag fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -155,7 +155,6 @@ export default function Home() {
                    Agendar Reunião
                 </a>
               </div>
-            </div>
           </div>
           
         </section>
