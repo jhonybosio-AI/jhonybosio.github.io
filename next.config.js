e6184bb923f8f465bdd85919e10328d04477b07a
Jhony Bosio <2fa.protegido@gmail.com>
2026-04-22 13:41:12 -0300

    FORCING REBUILD: Limpando cache e forçando novo build para ativar rota /moabi.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Forçando re-build do zero para o Moabi Dashboard
+  env: {
+    REBUILD_TIME: '2026-04-22T13:41:00Z'
+  }
 };
 
 module.exports = nextConfig;
